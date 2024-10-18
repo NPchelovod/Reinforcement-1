c2cd3f515a099335b5fce474492980b31e692c9d
KMedvedev <vadimmedvedev240@gmail.com>
2024-10-18 08:54:07 +0700

    Add code to Build MSI

--- BuildPlugin/Program.cs
+++ BuildPlugin/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,13 @@ namespace BuildPlugin
 {
     internal class Program
     {
-        private static string projectName = "Revit 2024 ENS plugin";
+        private static string projectName = "Revit ENS plugin";
         private static string version = "1.0";
         static void Main(string[] args)
         {
             var project = new Project()
             {
+                OutFileName = "ENS plugin 2024",
                 Name = projectName,
                 UI = WUI.WixUI_ProgressOnly,
                 OutDir = "output",
@@ -31,12 +33,34 @@ namespace BuildPlugin
                     new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\2024\",
                         new File(@"C:\Users\KMedvedev\source\repos\Reinforcement\Reinforcement\Reinforcement.addin"),
                         new Dir(@"ENSPlugin",
+                        new DirFiles(@"C:\Users\KMedvedev\source\repos\Reinforcement\Reinforcement\bin\Debug\*.*"))),
+
+                },
+            };
+            project.Version = new Version(version);
+            project.BuildMsi();
+
+            project = new Project()
+            {
+                OutFileName = "ENS plugin 2021",
+                Name = projectName,
+                UI = WUI.WixUI_ProgressOnly,
+                OutDir = "output",
+                GUID = new Guid("{D9449A17-6853-4EDF-8093-E8E9EC6EC084}"),
+                MajorUpgrade = MajorUpgrade.Default,
+                ControlPanelInfo =
+                {
+                    Manufacturer = Environment.UserName,
+                },
+                Dirs = new Dir[]
+                {
+                    new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\2021\",
+                        new File(@"C:\Users\KMedvedev\source\repos\Reinforcement\Reinforcement\Reinforcement.addin"),
+                        new Dir(@"ENSPlugin",
                         new DirFiles(@"C:\Users\KMedvedev\source\repos\Reinforcement\Reinforcement\bin\Debug\*.*")))
                 },
             };
-
             project.Version = new Version(version);
-
             project.BuildMsi();
         }
     }
