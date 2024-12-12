d486fc9057c84c85e3366bd4e31be5ea4b908eda
Вадим Медведев <karatist300@gmail.com>
2024-12-12 13:11:15 +0700

    • Добавлена команда для проверки направлений осей

---
+++ Reinforcement/Test/CheckGridsDirection.cs
@@ -0,0 +1,71 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reinforcement
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class CheckGridsDirection : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            if (RevitAPI.UiApplication == null)
+            {
+                RevitAPI.Initialize(commandData);
+            }
+            UIApplication uiapp = RevitAPI.UiApplication;
+            UIDocument uidoc = RevitAPI.UiDocument;
+            Document doc = RevitAPI.Document;
+            Selection sel = uidoc.Selection;
+
+            var gridIds = sel.GetElementIds();
+
+            string text = "";
+
+            foreach (var gridId in gridIds)
+            {
+                Grid grid = doc.GetElement(gridId) as Grid;
+                string name = grid.Name;
+                Line curve = (Line)grid.Curve;
+                string direction = curve.Direction.ToString();
+
+                text = string.Concat(text,"  ", name, direction, "\n");
+            }
+
+
+            MessageBox.Show(text);
+
+            try //ловим ошибку
+            {
+                using (Transaction t = new Transaction(doc, "действие"))
+                {
+                    t.Start();
+                    //Тут пишем основной код для изменения элементов модели
+                    t.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Код в случае ошибки
+                MessageBox.Show("Чет пошло не так!\n" + ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
+;
