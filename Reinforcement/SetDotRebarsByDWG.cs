39eee1398b49b1ee1b8b7c17ce24556104a6ac20
KBaceko <victor_batseko@mail.ru>
2025-02-11 13:50:32 +0700

    Small fix

---
+++ Reinforcement/SetDotRebarsByDWG.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reinforcement
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class OpenInstructionsFolder : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            // Укажите путь к папке, которую вы хотите открыть
+            string folderPath = @"Y:\Revit\_ШаблоныРегламентыЕнисейстрой\BIM-стандарт, инструкции";
+            string arguments = "\"" + folderPath + "\"";
+            try
+            {
+                // Открываем папку в проводнике
+                Process.Start("explorer.exe", arguments);
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                // В случае ошибки выводим сообщение
+                MessageBox.Show("Не удалось открыть папку: " + ex.Message);
+                return Result.Failed;
+            }
+        }
+    }
+}
