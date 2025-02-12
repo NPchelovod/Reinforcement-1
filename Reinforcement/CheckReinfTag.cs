9f00e61d7c5a001e79462cd8b2da42f8fc67042a
Вадим Медведев <karatist300@gmail.com>
2025-02-12 12:00:02 +0700

    •improve checkreinftag

--- Reinforcement/CheckReinfTag.cs
+++ Reinforcement/CheckReinfTag.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
@@ -19,6 +20,7 @@ namespace Reinforcement
 
     public class CheckReinfTag : IExternalCommand
     {
+        public static OverrideGraphicSettings halfToneTrue = new OverrideGraphicSettings().SetHalftone(true);
         public Result Execute(
             ExternalCommandData commandData,
             ref string message,
@@ -33,11 +35,9 @@ namespace Reinforcement
             Document doc = RevitAPI.Document;
             Selection sel = uidoc.Selection;
 
-
-            
             try //ловим ошибку
             {
-                var reinforcmentListId = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                List <ElementId> reinforcmentListId = new FilteredElementCollector(doc, doc.ActiveView.Id)
                 .OfClass(typeof(FamilyInstance))
                 .OfCategory(BuiltInCategory.OST_DetailComponents)
                 .ToElements()
@@ -66,121 +66,150 @@ namespace Reinforcement
                     .ToList();
 
                 List<ElementId> noTagElement = reinforcmentListId.Except(taggedElementsIds).ToList();
-
-
-                if (doubleTaggedElementsIds.Count != 0)
+                using (TransactionGroup tg = new TransactionGroup(doc, "Проверка выносок арматуры"))
                 {
-                    DialogResult dialogResult = MessageBox.Show("Показать задвоенные выноски?" ,"Выбор" , MessageBoxButtons.YesNo);
-
-                    if (dialogResult == DialogResult.Yes)
+                    tg.Start();
+                    using (Transaction t = new Transaction(doc, "Переопределение графики"))
                     {
-                        foreach (var element in doubleTaggedElementsIds)
+                        t.Start();
+                        if (doubleTaggedElementsIds.Count != 0 || noTagElement.Count != 0)
                         {
-                            uidoc.ShowElements(element);
-                            sel.SetElementIds(new List<ElementId> { element });
-                            dialogResult = MessageBox.Show("Показать далее?", "Выбор", MessageBoxButtons.YesNo);
+                            List<ElementId> listHideElements = reinforcmentListId
+                            .Except(noTagElement)
+                            .Except(doubleTaggedElementsIds)
+                            .SelectMany(x =>
+                            {
+                                var fam = doc.GetElement(x) as FamilyInstance;
+                                return fam.GetSubComponentIds().Append(x);
+                            })
+                            .ToList();
+
+                            doc.ActiveView.HideElementsTemporary(listHideElements);
+
+                            /*DialogResult dialogResult = MessageBox.Show("Показать задвоенные выноски?" ,"Выбор" , MessageBoxButtons.YesNo);
+
                             if (dialogResult == DialogResult.Yes)
                             {
-                                continue;
-                            }
-                            else
-                            {
-                                break;
-                            }
+                                foreach (var element in doubleTaggedElementsIds)
+                                {
+                                    uidoc.ShowElements(element);
+                                    sel.SetElementIds(new List<ElementId> { element });
+                                    SetHalfTone(element, reinforcmentListId, doc, t);
+                                    dialogResult = MessageBox.Show("Показать далее?", "Выбор", MessageBoxButtons.YesNo);
+                                    if (dialogResult == DialogResult.Yes)
+                                    {
+                                        ResetOvveride(element, reinforcmentListId, doc, t);
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        ResetOvveride(element, reinforcmentListId, doc, t);
+                                        break;
+                                    }
+                                }
+                            }*/
                         }
-                    }
-                }
-                if (noTagElement.Count != 0)
-                {
-                    DialogResult dialogResult = MessageBox.Show("Показать не замаркированные?", "Выбор", MessageBoxButtons.YesNo);
-                    if (dialogResult == DialogResult.Yes)
-                    {
-                        foreach (var element in noTagElement)
-                        {
-                            uidoc.ShowElements(element);
-                            sel.SetElementIds(new List<ElementId> { element });
-                            dialogResult = MessageBox.Show("Показать далее?", "Выбор", MessageBoxButtons.YesNo);
+                        if (noTagElement.Count != 0)
+                        {                       
+                            /*
+                            DialogResult dialogResult = MessageBox.Show("Показать не замаркированные?", "Выбор", MessageBoxButtons.YesNo);
                             if (dialogResult == DialogResult.Yes)
                             {
-                                continue;
-                            }
-                            else
-                            {
-                                break;
-                            }
+                                foreach (var element in noTagElement)
+                                {
+                                    uidoc.ShowElements(element);
+                                    sel.SetElementIds(new List<ElementId> { element });
+                                    SetHalfTone(element, reinforcmentListId, doc, t);
+                                    dialogResult = MessageBox.Show("Показать далее?", "Выбор", MessageBoxButtons.YesNo);
+                                    if (dialogResult == DialogResult.Yes)
+                                    {
+                                        ResetOvveride(element, reinforcmentListId, doc, t);
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        ResetOvveride(element, reinforcmentListId, doc, t);
+                                        break;
+                                    }
+                                }
+                            }*/
                         }
-                    }
-                }
-                if (noTagElement.Count == 0 && doubleTaggedElementsIds.Count == 0)
-                {
-                    MessageBox.Show("Все ок");
-                }
-
-                using (Transaction t = new Transaction(doc, "Выровнять выноски"))
-                {
-                    t.Start();
-                    //Тут пишем основной код для изменения элементов модели
-                    ElementClassFilter filter = new ElementClassFilter(typeof(FamilyInstance));
-
-                    if (alignTagEnd == DialogResult.Yes)
-                    {
-                        foreach (IndependentTag tag in tags)
+                        if (noTagElement.Count == 0 && doubleTaggedElementsIds.Count == 0)
                         {
-                            if (tag.HasLeader && tag.MultiLeader)
-                            {
-                                List<LocationPoint> locations;
-                                try
-                                {
-                                    locations = tag.GetTaggedLocalElements().DefaultIfEmpty()
-                                    .Cast<FamilyInstance>()
-                                    .Select(x => x.GetDependentElements(filter))
-                                    .SelectMany(x => x)
-                                    .Select(x => doc.GetElement(x))
-                                    .Where(x => x.Name == "Точка")
-                                    .Select(x => x.Location as LocationPoint)
-                                    .ToList();
-                                }
-                                catch (NullReferenceException)
-                                {
-                                    continue;
-                                }
-                                var points = locations.Select(x => x.Point).ToList();
-                                var references = tag.GetTaggedReferences();
-                                for (int i = 0; i < references.Count; i++)
-                                {
-                                    tag.SetLeaderEnd(references[i], points[i]);
-                                }
-                            }
-                            else if (tag.HasLeader)
-                            {
-                                LocationPoint location;
-                                try
-                                {
-                                     location = tag.GetTaggedLocalElements().FirstOrDefault()
-                                    .GetDependentElements(filter)
-                                    .Select(x => doc.GetElement(x))
-                                    .Where(x => x.Name == "Точка")
-                                    .FirstOrDefault()
-                                    .Location as LocationPoint;
-                                }
-                                catch (NullReferenceException)
-                                {
-                                    continue;
-                                }
-                                var point = location.Point;
-                                tag.SetLeaderEnd(tag.GetTaggedReferences().First(), point);
-                            }
+                            MessageBox.Show("Все ок");
                         }
+                        t.Commit();
                     }
 
+                    using (Transaction t = new Transaction(doc, "Выровнять выноски"))
+                    {
+                        t.Start();
+                        //Тут пишем основной код для изменения элементов модели
+                        ElementClassFilter filter = new ElementClassFilter(typeof(FamilyInstance));
 
-                    t.Commit();
+                        if (alignTagEnd == DialogResult.Yes)
+                        {
+                            foreach (IndependentTag tag in tags)
+                            {
+                                if (tag.HasLeader && tag.MultiLeader)
+                                {
+                                    List<LocationPoint> locations;
+                                    try
+                                    {
+                                        locations = tag.GetTaggedLocalElements().DefaultIfEmpty()
+                                        .Cast<FamilyInstance>()
+                                        .Select(x => x.GetDependentElements(filter))
+                                        .SelectMany(x => x)
+                                        .Select(x => doc.GetElement(x))
+                                        .Where(x => x.Name == "Точка")
+                                        .Select(x => x.Location as LocationPoint)
+                                        .ToList();
+                                    }
+                                    catch (NullReferenceException)
+                                    {
+                                        continue;
+                                    }
+                                    var points = locations.Select(x => x.Point)?.ToList();
+                                    if (points == null || points.Count == 0)
+                                        continue;
+                                    var references = tag.GetTaggedReferences();
+                                    for (int i = 0; i < references.Count; i++)
+                                    {
+                                        tag.SetLeaderEnd(references[i], points[i]);
+                                    }
+                                }
+                                else if (tag.HasLeader)
+                                {
+                                    LocationPoint location;
+                                    try
+                                    {
+                                        location = tag.GetTaggedLocalElements().FirstOrDefault()
+                                       .GetDependentElements(filter)
+                                       .Select(x => doc.GetElement(x))
+                                       .Where(x => x.Name == "Точка")
+                                       .FirstOrDefault()
+                                       .Location as LocationPoint;
+                                    }
+                                    catch (NullReferenceException)
+                                    {
+                                        continue;
+                                    }
+                                    var point = location.Point;
+                                    tag.SetLeaderEnd(tag.GetTaggedReferences().First(), point);
+                                }
+                            }
+                        }
+
+
+                        t.Commit();
+                    }
+                    tg.Assimilate();
                 }
             }
             catch (Exception ex)
             {
                 //Код в случае ошибки
-                MessageBox.Show("Чет пошло не так!\n" + ex.Message);
+                MessageBox.Show("Чет пошло не так!\n" + ex.Message + ex.StackTrace);
                 return Result.Failed;
 
             }
