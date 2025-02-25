474c4d2ebbdb8e8ad72e9df894e4dd731bb23c82
Вадим Медведев <karatist300@gmail.com>
2025-02-25 19:56:04 +0700

    commit

---
+++ Reinforcement/ReinforcementList/ReinforcementList.cs
@@ -0,0 +1,92 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reinforcement
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class ReinforcementList : IExternalCommand
+    {
+        public static string ConstrMark { get; set; }
+        public static string ElementMark { get; set; }
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            if (RevitAPI.UiApplication == null)
+            {
+                RevitAPI.Initialize(commandData);
+            }
+            UIApplication uiapp = RevitAPI.UiApplication;
+            UIDocument uidoc = RevitAPI.UiDocument;
+            Document doc = RevitAPI.Document;
+
+            var dialog = new MainViewReinforcementList();
+            var result = dialog.ShowDialog();
+            if (String.IsNullOrEmpty(ConstrMark) || String.IsNullOrEmpty(ElementMark) || result == false)
+            {
+                return Result.Cancelled;
+            }
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            var reinfList = collector.
+                OfClass(typeof(FamilyInstance))
+                .OfCategory(BuiltInCategory.OST_DetailComponents)
+                .Cast<FamilyInstance>()
+                .Where(x => (x.LookupParameter("◦ Марка конструкции")?.AsValueString() ?? "").Contains(ConstrMark))
+                .Where(x =>
+                        (x.LookupParameter("• Считать Г-шки")?.AsInteger() ?? 0) == 1 ||
+                        (x.LookupParameter("• Считать П-шки")?.AsInteger() ?? 0) == 1 ||
+                        (x.LookupParameter("• Учет в спецификации")?.AsInteger() ?? 0) == 1
+                      )
+                .Where(x => x.SuperComponent == null)
+                .Where(x => (x.LookupParameter("• Марка элемента")?.AsValueString() ?? "").Contains(ElementMark))
+                .Select(x => x.Id)              
+                .ToList();
+
+
+
+            try //ловим ошибку
+            {
+                using (Transaction t = new Transaction(doc, "действие"))
+                {
+                    foreach (var reinf in reinfList)
+                    {
+                        var element = doc.GetElement(reinf);
+                        var viewId = element.OwnerViewId;
+                        var view = doc.GetElement(viewId) as Autodesk.Revit.DB.View;
+                        uidoc.ActiveView = view;
+                        t.Start();
+                        doc.ActiveView.IsolateElementsTemporary(reinfList);
+                        t.Commit();
+                    }
+
+                    t.Start();
+                    //Тут пишем основной код для изменения элементов модели
+                    Selection sel = uidoc.Selection;
+                    sel.SetElementIds(reinfList);
+                    t.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Код в случае ошибки
+                MessageBox.Show("Чет пошло не так!\n" + ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
--- Reinforcement/Test/SetParamReinfByElementMark.cs
+++ Reinforcement/Test/SetParamReinfByElementMark.cs
@@ -12,11 +12,11 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Reinforcement
+namespace Reinforcement.Test
 {
     [Transaction(TransactionMode.Manual)]
 
-    public class ReinforcementTemplate1 : IExternalCommand
+    public class SetParamReinfByElementMark : IExternalCommand
     {
         public Result Execute(
             ExternalCommandData commandData,
@@ -30,13 +30,40 @@ namespace Reinforcement
             UIApplication uiapp = RevitAPI.UiApplication;
             UIDocument uidoc = RevitAPI.UiDocument;
             Document doc = RevitAPI.Document;
+
+            string elementMark = "Ск";
+            var familyInstances1 = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfClass(typeof(FamilyInstance))
+                .OfCategory(BuiltInCategory.OST_DetailComponents)
+                .Cast<FamilyInstance>()
+                .Where(x => (x.LookupParameter("• Марка элемента")?.AsValueString() ?? "").Contains(elementMark))
+                .Where(x => x.SuperComponent == null)
+                .ToList();
+
+            elementMark = "М";
+            var familyInstances2 = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfClass(typeof(FamilyInstance))
+                .OfCategory(BuiltInCategory.OST_DetailComponents)
+                .Cast<FamilyInstance>()
+                .Where(x => (x.LookupParameter("• Марка элемента")?.AsValueString() ?? "").Contains(elementMark))
+                .Where(x => x.SuperComponent == null)
+                .ToList();
+
+
             try //ловим ошибку
             {
                 using (Transaction t = new Transaction(doc, "действие"))
                 {
                     t.Start();
                     //Тут пишем основной код для изменения элементов модели
-
+                    foreach (var familyInstance in familyInstances1)
+                    {
+                        familyInstance.LookupParameter("• Считать П-шки").Set(1);
+                    }
+                    foreach (var familyInstance in familyInstances2)
+                    {
+                        familyInstance.LookupParameter("• Считать Г-шки").Set(1);
+                    }
                     t.Commit();
                 }
             }
