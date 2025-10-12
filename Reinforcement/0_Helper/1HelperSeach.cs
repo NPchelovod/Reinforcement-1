3bcca706667e6f4aa40e079174ecce1960f6dbb9
NPchelovod <78476760+NPchelovod@users.noreply.github.com>
2025-10-12 17:53:46 +0700

    com ускорение поиска

--- Reinforcement/0_Helper/1HelperSeach.cs
+++ Reinforcement/0_Helper/1HelperSeach.cs
@@ -15,6 +15,17 @@ namespace Reinforcement
     {
         private const double StopSovpad = 0.95;// с такой долей слова считаются одинаковыми
 
+        private static Dictionary<string, ElementId> familyNames = new Dictionary<string, ElementId>();
+        private static Document docPast = null;
+        private static int pastFamily = 0;
+
+        private static int popitka = 0;
+        private static int popitkamax = 4;
+
+        //словарь ускорений
+        private static Dictionary<HashSet<string>, ElementId> PastExistData = new Dictionary<HashSet<string>, ElementId>();
+
+
         public static (ElementId pileId, HashSet<string> PossibleNames)  GetExistFamily(HashSet<string> PossibleNames, ExternalCommandData commandData )
         {
             //надо например расставить сваи по виду, вот находит по имени совпадению сваю эту наиболее близкую id и возвращает ее
@@ -26,158 +37,109 @@ namespace Reinforcement
 
 
             ElementId pileId = null;
-            FilteredElementCollector collection = new FilteredElementCollector(doc);
+            var collection = new FilteredElementCollector(doc).OfClass(typeof(Family));
 
             string familyName = null;
             bool famExist = false;
-            foreach (string PossibleName in PossibleNames)
+
+            //чтобы по многу раз не находить
+            if (familyNames.Count == 0 || collection.Count()!= pastFamily || docPast != doc || popitka > popitkamax)
             {
-                //Проверка есть ли в проекте "ЕС_Буронабивная свая"
-                famExist = collection
-                    .OfClass(typeof(Family))
-                    .Cast<Family>()
-                    .Any(family => family.Name.Equals(PossibleName));
-                if (famExist)
+                familyNames.Clear();
+                PastExistData.Clear();
+                foreach (var element in collection)
                 {
-                    familyName = PossibleName;
-                    break;
+                    familyNames[element.Name] = element.Id;
+                }
+                popitka = 0;
+                pastFamily = collection.Count();
+                docPast = doc;
+            }
+            else
+            {
+                popitka++;
+                if (PastExistData.TryGetValue(PossibleNames,out pileId))
+                {
+                    famExist=true;// из словаря найти прошлое
                 }
             }
 
-            if (famExist) 
-            {
-                //ищем id
-                // Продолжить с точным совпадением //Типоразмер нужной сваи
-                pileId = collection.OfClass(typeof(Family))
-                    .Where(x => x.Name.Equals(familyName, StringComparison.OrdinalIgnoreCase))
-                    .Cast<Family>()
-                    .FirstOrDefault()
-                    .GetFamilySymbolIds()
-                    .FirstOrDefault();
-            }
+            
 
-            if (!famExist)// не нашли в лоб
+            HashSet<string> PossibleNamesChange = new HashSet<string>(PossibleNames);
+            int circle = -1;
+            while (!famExist && circle < 7)
             {
-                // ищем так
-                // Нечеткий поиск, если точный не дал результатов
-                var sravnData = collection
-                    .OfClass(typeof(Family))
-                    .Cast<Family>();
+                circle++;
                 double maxSimilarity = 0;
                 string familyNameMax = "";
                 ElementId pileIdMax = null;
-                foreach (string PossibleName in PossibleNames)
+
+                foreach (string PossibleName in PossibleNamesChange)
                 {
-                    //Family foundFamily = sravnData
-                    //.Select(family => new { Family = family, Similarity = HelperPrivateStatic.CalculateSimilarity(familyName, family.Name) })
-                    //.Where(x => x.Similarity >= 0.7) // Пороговое значение схожести 70%
-                    //.OrderByDescending(x => x.Similarity)
-                    //.FirstOrDefault()?.Family;
-                    foreach (var family in sravnData)
+                    //Проверка есть ли в проекте "ЕС_Буронабивная свая"
+                    foreach (var name in familyNames.Keys)
                     {
-                        var Similarity = HelperPrivateStatic.CalculateSimilarity(PossibleName, family.Name);
-                        if (Similarity > maxSimilarity)
+
+                        var Similarity = HelperPrivateStatic.CalculateSimilarity(PossibleName, name);
+                        if (Similarity>0.7 && Similarity > maxSimilarity)
                         {
                             maxSimilarity = Similarity;
-                            familyNameMax = family.Name;
-                            pileIdMax = family.Id;
+                            familyNameMax = name;
+                            pileIdMax = familyNames[name];
                             //!!!!!!!! остановка чтоб не ходить много
-                            if (maxSimilarity > StopSovpad)
-                            { break; }
+                            if (maxSimilarity > 0.99 && name.Count() > 6)
+                            {
+                                famExist = true;
+                                familyName = familyNameMax;
+                                pileId = pileIdMax;
+                                break; 
+                            }
                         }
                     }
-
                 }
 
-
+                if (famExist)
+                {
+                    break;
+                }
 
                 // Спросить пользователя о использовании найденного семейства
-                if (maxSimilarity < StopSovpad)
-                {
-                    // Спросить пользователя о использовании найденного семейства
-                    DialogResult result = MessageBox.Show(
-                    $"Точное семейство '{PossibleNames.FirstOrDefault()}' не найдено. Использовать '{familyNameMax}'?",
-                    "Семейство не найдено",
-                    MessageBoxButtons.YesNo);
-                    if (result == DialogResult.Yes)
-                    {
-                        famExist = true;
-                        familyName = familyNameMax;
-                        pileId = pileIdMax;
-                        PossibleNames.Add(familyName);
-                    }
+                DialogResult result = MessageBox.Show(
+                $"Точное семейство '{PossibleNames.FirstOrDefault()}' не найдено. Использовать '{familyNameMax}'?",
+                "Семейство не найдено",
+                MessageBoxButtons.YesNo);
 
-                    else
-                    {
-                        famExist = false;
-                    }
-                }
-                else
+                if (result == DialogResult.Yes && maxSimilarity>0.2)
                 {
                     famExist = true;
                     familyName = familyNameMax;
                     pileId = pileIdMax;
                     PossibleNames.Add(familyName);
+                    break;
                 }
+                int circle2 = -1;
 
-
-                if (!famExist)
+                bool proxod=true;
+                while (circle2<5)
                 {
-                    //предлагаем ввести вручную
-                    while (true)
+                    circle2++;
+                    var input = HelperPrivateStatic.GetUserInputWithForm();
+                    if (!input.Item2) { proxod = false; break; }
+                    PossibleNamesChange.Clear();
+
+                    if (input.Item1.Count() > 5)
                     {
-                        var input = HelperPrivateStatic.GetUserInputWithForm();
-                        if (!input.Item2) { break; }
-                        string PossibleName = input.Item1;
-
-                        maxSimilarity = 0;
-                        foreach (var family in sravnData)
-                        {
-                            var Similarity = HelperPrivateStatic.CalculateSimilarity(PossibleName, family.Name);
-                            if (Similarity > maxSimilarity)
-                            {
-                                maxSimilarity = Similarity;
-                                familyNameMax = family.Name;
-                                pileIdMax = family.Id;
-                                if (maxSimilarity > StopSovpad)
-                                { break; }
-                            }
-
-                        }
-
-                        if (maxSimilarity < StopSovpad)
-                        {
-                            DialogResult result = MessageBox.Show(
-                            $"Точное семейство '{PossibleName}' не найдено. Использовать '{familyNameMax}'?",
-                            "Семейство не найдено",
-                            MessageBoxButtons.YesNo);
-                            if (result == DialogResult.Yes)
-                            {
-                                famExist = true;
-                                familyName = familyNameMax;
-                                pileId = pileIdMax;
-                                PossibleNames.Add(familyName);
-                                break;
-                            }
-                            else
-                            {
-                                famExist = false;
-                            }
-                        }
-                        else
-                        {
-                            famExist = true;
-                            familyName = familyNameMax;
-                            pileId = pileIdMax;
-                            PossibleNames.Add(familyName);
-                            break;
-                        }
-
+                        PossibleNamesChange.Add(input.Item1);
+                        break;
                     }
-
                 }
-
+                if(!proxod)
+                {
+                    break;
+                }
             }
+
             if (!famExist)
             {
                 pileId = null;
@@ -185,6 +147,12 @@ namespace Reinforcement
             }
             else
             {
+                if (familyName!=null && !PossibleNames.Contains(familyName))
+                { 
+                    PossibleNames.Add(familyName);
+                
+                }
+                PastExistData[PossibleNames] = pileId;
                 return (pileId, PossibleNames);
             }
 
