1e23004caef4ebdbf404a8fc922aa90d908a0c76
NPchelovod <78476760+NPchelovod@users.noreply.github.com>
2026-09-16 15:42:59 +0700

    Для ЭЛ добавлены всякие кнопки по предложению Молочкова

---
+++ Reinforcement/3_Electric/2_3_EL_Vvod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Reinforcement
+{
+    [Transaction(TransactionMode.Manual)]
+    public class EL_Vvod : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            RevitAPI.Initialize(commandData);
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            Document doc = uidoc.Document;
+
+
+
+            ElementTypeOrSymbol Type_seach = ElementTypeOrSymbol.Symbol;
+
+            try
+            {
+                Utilit_1_1_Depth_Seach.GetResult(list_Name, Type_seach);
+            }
+            catch (Exception)
+            {
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+        public static HashSet<string> list_Name = new HashSet<string>
+        {
+            "ЕС_Закладная ввода"
+        };
+        public static HashSet<string> Type_Name = new HashSet<string>
+        {
+            "А500С"
+        };
+    }
+}
--- Reinforcement/App_Panel_5_2_EL_utilit.cs
+++ Reinforcement/App_Panel_5_2_EL_utilit.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.UI;
+﻿using System.Linq;
+using Autodesk.Revit.UI;
 
 namespace Reinforcement
 {
@@ -11,6 +12,21 @@ namespace Reinforcement
                  "Позволяет заменить кубики КУ1301 на светильники и патроны",
                  "Для работы плагина нужно иметь в проекте кубики",
                 panel);
+
+            App_Helper_Button.CreateButton("Гусак", "Гусак", "Reinforcement.EL_GusakRoof", Properties.Resources.GRebar,
+                "Размещение Гусака", $"Имя семейства должно быть {EL_GusakRoof.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Закладная\nплиты", "Закладная\nплиты", "Reinforcement.ZacladInSlab", Properties.Resources.ES_RebarFromSide,
+                "Размещение Закладная\nплиты", $"Имя семейства должно быть {ZacladInSlab.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Закладная\nввода", "Закладная\nввода", "Reinforcement.EL_Vvod", Properties.Resources.Arrow_of_view,
+                "Размещение Закладная ввода", $"Имя семейства должно быть {EL_Vvod.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Кубик\nплита круг", "Кубик\nплита круг", "Reinforcement.CubicCircle", Properties.Resources.ES_RebarInFront,
+                "Размещение Кубик_Перекрытие_Круг", $"Имя семейства должно быть {CubicCircle.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Кубик\nплита прям", "Кубик\nплита прям", "Reinforcement.CubicSlabRectangle", Properties.Resources.HomutDistrib,
+                "Размещение Кубик_Перекрытие_Прямоугольный", $"Имя семейства должно быть {CubicSlabRectangle.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Кубик\nстена круг", "Кубик\nстена круг", "Reinforcement.CubicWallCircle", Properties.Resources.ES_RebarInFront,
+                "Размещение Кубик_Стена_Круг", $"Имя семейства должно быть {CubicWallCircle.list_Name.FirstOrDefault()}", panel);
+            App_Helper_Button.CreateButton("Кубик\nстена прям", "Кубик\nстена прям", "Reinforcement.CubicWallRectangle", Properties.Resources.HomutDistrib,
+                "Размещение Кубик_Стена_Прямоугольный", $"Имя семейства должно быть {CubicWallRectangle.list_Name.FirstOrDefault()}", panel);
 
         }
     }
